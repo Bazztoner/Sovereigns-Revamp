6957636bac4451dc1a21bca9c8f207243208ef18
Iván Piccione <bazztoner.phoenix@gmail.com>
2017-09-01 05:02:38 -0300

    Me olvidé de pushear este script, es para desactivar los gráficos de los triggers y spawnpoints de las trancisiones

---
+++ Assets/Scripts/Temp/LevelTransitions/Subdivisions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subdivisions : MonoBehaviour
+{
+    public bool useGraphics = true;
+
+    void Start ()
+    {
+        if (useGraphics)
+        {
+            var zones = GetComponentsInChildren<Transform>();
+            foreach (Transform z in zones)
+            {
+                var r = z.GetComponent<Renderer>();
+                if (r != null )
+                {
+                    r.enabled = true;
+                }
+               
+            }
+        }
+        if (!useGraphics)
+        {
+            var zones = GetComponentsInChildren<Transform>();
+            foreach (Transform z in zones)
+            {
+                var r = z.GetComponent<Renderer>();
+                if (r != null)
+                {
+                    r.enabled = false;
+                }
+
+            }
+        }
+    }
+	
+	void Update ()
+    {
+    }
+}
