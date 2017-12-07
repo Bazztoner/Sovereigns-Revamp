aff9e6c9c4a40a17ba075aef2bdeeb762a7e44f8
Admin-PC\Admin <pablo.maldonado.navone@gmail.com>
2017-12-06 23:54:45 -0300

    Menu implementado.

--- Assets/Scripts/HUD/Menu.cs
+++ Assets/Scripts/HUD/Menu.cs
@@ -5,20 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public Light angelLight;
-    public Light demonLight;
-    public GameObject button;
+    //public Light angelLight;
+    //public Light demonLight;
+    public GameObject mainScreen;
     public GameObject loading;
-    public void ChangeScene()
+    public GameObject credits;
+
+    public void ChangeToGameScene()
     {
         StartCoroutine(SceneLoader());
     }
 
+    public void ChangeToCredits()
+    {
+        credits.SetActive(true);
+        mainScreen.SetActive(false);
+    }
+
+    public void ChangeBackToMenu()
+    {
+        mainScreen.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+
+
     IEnumerator SceneLoader()
     {
-        yield return null;
-
-        button.SetActive(false);
+        mainScreen.SetActive(false);
         loading.SetActive(true);
 
         AsyncOperation ao = SceneManager.LoadSceneAsync("TransitionsTest", LoadSceneMode.Single);
@@ -27,8 +45,8 @@ public class Menu : MonoBehaviour
         while (!ao.isDone)
         {
             float progress = Mathf.Clamp01(ao.progress / 0.9f);
-            angelLight.intensity = Mathf.Lerp(angelLight.intensity, 0.2f, progress);
-            demonLight.intensity = Mathf.Lerp(demonLight.intensity, 5.3f, progress);
+            //angelLight.intensity = Mathf.Lerp(angelLight.intensity, 0.2f, progress);
+            //demonLight.intensity = Mathf.Lerp(demonLight.intensity, 5.3f, progress);
 
             if (ao.progress >= 0.9f) ao.allowSceneActivation = true;
 
